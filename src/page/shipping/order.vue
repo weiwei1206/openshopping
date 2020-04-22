ec06ab6a36f5165fe9469924cec15d230f64e5e6
jamiory <1970858630@qq.com>
2020-04-22 21:31:43 +0800

    order修正

--- src/page/shipping/order.vue
+++ src/page/shipping/order.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="order">
-     <headerNav title="确认订单"/>
-     <van-cell
+    <headerNav title="确认订单" />
+    <van-cell
       center
       :border="false"
       class="contact-card"
@@ -11,38 +11,56 @@
       <template v-if="type === false">
         <strong @click="chooseAddr">选择地址</strong>
       </template>
-      <template  v-else>
-        <strong >{{address[0].aname}} {{address[0].atele}}</strong>
+      <template v-else>
+        <strong>{{address[0].aname}} {{address[0].atele}}</strong>
         <div>{{address[0].address}}</div>
       </template>
     </van-cell>
     <div style="height:15px;"></div>
-    <div class="card" v-for="(product,i) in products" :key="i">
+    <div
+      class="card"
+      v-for="(product,i) in products"
+      :key="i"
+    >
       <product-card :product='product' />
     </div>
-        <div style="height:15px;"></div>
-            <van-cell-group>
-            <van-field
-                label="留言"
-                type="textarea"
-                placeholder="请输入留言"
-                rows="1"
-                v-model="curOrder[0].comments"
-                autosize
-            />
-            </van-cell-group>
-        <div style="height:15px;"></div>
-    
+    <div style="height:15px;"></div>
+    <van-cell-group>
+      <van-field
+        label="留言"
+        type="textarea"
+        placeholder="请输入留言"
+        rows="1"
+        v-model="curOrder[0].comments"
+        autosize
+      />
+    </van-cell-group>
+    <div style="height:15px;"></div>
+
     <div style="height:15px;"></div>
     <van-cell-group class="total">
-        <van-cell title="商品总额" :value='totalPrice/100' />
-        <van-cell title="运费" :value="Freight" />
-        <van-cell title="折扣" :value='discount' />
-        <van-cell title="实付金额" :value="totalPrice/100+Freight-discount" style="font-weight: 700;" />
+      <van-cell
+        title="商品总额"
+        :value='totalPrice/100'
+      />
+      <van-cell
+        title="运费"
+        :value="Freight"
+      />
+      <van-cell
+        title="折扣"
+        :value='discount'
+      />
+      <van-cell
+        title="实付金额"
+        :value="totalPrice/100+Freight-discount"
+        style="font-weight: 700;"
+      />
     </van-cell-group>
 
     <div style="height:50px;"></div>
-    <van-submit-bar id="submitOrder"
+    <van-submit-bar
+      id="submitOrder"
       :price="totalPrice+Freight*100-discount*100"
       button-text="提交订单"
       label='实付金额：'
@@ -57,13 +75,13 @@ export default {
   data() {
     return {
       type: true,
-      discount:100,//折扣
-      Freight:10,//运费
-      showTip:true,//默认从该页面退出需要显示确认信息，但是已经下单就不需要了
-      address:[{
-        address:" ",
-        atele:" ",
-        aname:' '
+      discount: 100,//折扣
+      Freight: 10,//运费
+      showTip: true,//默认从该页面退出需要显示确认信息，但是已经下单就不需要了
+      address: [{
+        address: " ",
+        atele: " ",
+        aname: ' '
       }],
       products: [
         {
@@ -91,163 +109,163 @@ export default {
           cnum: 2
         },
       ],
-      curOrder:[
+      curOrder: [
         {
           oid: 1,
-          ogoods_id:1,
+          ogoods_id: 1,
           onum: 10,
           oprice: 2000,
-          oaddr_id:1,
-          otime:"2020-04-01T08:18:03.834Z",
-          ostate:"待付款",
-          comments:""
+          oaddr_id: 1,
+          otime: "2020-04-01T08:18:03.834Z",
+          ostate: "待付款",
+          comments: ""
         }
       ]
     };
   },
   methods: {
-    createOrder(state){
-        //生成订单
-        var date = new Date();
-        var year = date.getFullYear();
-        var month = date.getMonth() + 1;
-        var day = date.getDate();
-        if (month < 10) {
-            month = "0" + month;
-        }
-        if (day < 10) {
-            day = "0" + day;
-        }
-        var mytime = year + "-" + month + "-" + day; 
-        this.curOrder[0].onum = this.count;
-        this.curOrder[0].oprice = this.totalPrice/100+this.Freight-this.discount;
-        this.curOrder[0].oaddr_id = 2;//需要修改，看传入的是什么商品
-        this.curOrder[0].ostate=state;
-        this.curOrder[0].otime = mytime;
+    createOrder(state) {
+      //生成订单
+      var date = new Date();
+      var year = date.getFullYear();
+      var month = date.getMonth() + 1;
+      var day = date.getDate();
+      if (month < 10) {
+        month = "0" + month;
+      }
+      if (day < 10) {
+        day = "0" + day;
+      }
+      var mytime = year + "-" + month + "-" + day;
+      this.curOrder[0].onum = this.count;
+      this.curOrder[0].oprice = this.totalPrice / 100 + this.Freight - this.discount;
+      this.curOrder[0].oaddr_id = 2;//需要修改，看传入的是什么商品
+      this.curOrder[0].ostate = state;
+      this.curOrder[0].otime = mytime;
     },
     onSubmit() {
-        //默认收货地址不为空才可以submit
-        if(this.address.address!=' '){
-          this.showTip=false;
-          console.log(this.curOrder[0]);
-          this.createOrder("待收货");
-          //提交订单信息
-          this.$axios.post("https://afc1gr.toutiao15.com/insertOrder",this.curOrder[0])
-          .then(res=>{
+      //默认收货地址不为空才可以submit
+      if (this.address.address != ' ') {
+        this.showTip = false;
+        console.log(this.curOrder[0]);
+        this.createOrder("待收货");
+        //提交订单信息
+        this.$axios.post("https://afc1gr.toutiao15.com/insertOrder", this.curOrder[0])
+          .then(res => {
             alert("下单成功");
             this.$router.push("/cart");
           })
-          .catch(err=>{
+          .catch(err => {
             alert("下单失败，请检查地址是否为空~");
             console.log(err);
           });
-          //删除原购物车中的商品
-          this.products.forEach((item)=>{
-            item.cnum = 0;
-            this.$axios.put('https://afc1gr.toutiao15.com/updateGoods',item)
-              .then(res=>{
-                  console.log(res);
-              })
-              .catch(err=>{
-                  console.log(err);
-              })
-          })
-        }else{
-          alert("地址为空，请填写收货地址！")
-        }
+        //删除原购物车中的商品
+        this.products.forEach((item) => {
+          item.cnum = 0;
+          this.$axios.put('https://afc1gr.toutiao15.com/updateGoods', item)
+            .then(res => {
+              console.log(res);
+            })
+            .catch(err => {
+              console.log(err);
+            })
+        })
+      } else {
+        alert("地址为空，请填写收货地址！")
+      }
       //this.$toast("点击按钮");
     },
-    chooseAddr(){
+    chooseAddr() {
       //跳转到地址页面，选择一个默认地址
       this.$axios.push("/address");
     }
   },
-    beforeRouteLeave(to, from, next){
-        //导航离开该组件的对应路由时调用
-        if(this.showTip===true){
-          //非正常退出，也就是还没有下单就想离开订单页面
-          var yes = confirm("是否保存订单？");
-          if(yes){
-            this.createOrder("待付款");
-                      //提交订单信息
-            this.$axios.post("https://afc1gr.toutiao15.com/insertOrder",this.curOrder[0])
-            .then(res=>{
-              alert("保存成功，请于待付款订单中查找~");
-              this.$router.push("/cart");
-            })
-            .catch(err=>{
-              alert("保存失败！");
-              console.log(err);
-            });
-            next();
-          }
-          else{
-            next();
-            console.log(to);
-          }
-        }else{//已经下过单了，可以直接跳转
-          next();
-        }
-    },
-computed: {
-        submitBarText() {
-            let count = 0;
-            this.products.forEach((shop)=>{
-                if(shop.isSelected) count++;
-            })
-            return '结算' + (count ? `(${count})` : '');
-        },
-        count(){
-          let count = 0;
-            this.products.forEach((shop)=>{
-                if(shop.isSelected) count++;
-            })
-            return count;
-        },
-        totalPrice() {
-            let total = 0;
-            this.products.forEach((shop)=>{
-                if(shop.isSelected) total += shop.cnum*shop.gprice;
-            })
-            return total*100;
-        }
-    },
-
-    created(){
-      this.$axios.get("https://afc1gr.toutiao15.com/getDefaultAddr")
-      .then(res=>{
-          //第一遍拿回来的数据是过期的
-          this.address = res.data.result;
-          //  console.log(this.address)
-          if(this.address.length>=1){
-            this.type = true;//有默认地址，就不显示添加地址
-            //var addr = document.getElementById("addrInfo");
-          }
-          else{
-            this.type = false;//否则需要添加地址
-          }
-          console.log(this.address);
+  beforeRouteLeave(to, from, next) {
+    //导航离开该组件的对应路由时调用
+    if (this.showTip === true) {
+      //非正常退出，也就是还没有下单就想离开订单页面
+      var yes = confirm("是否保存订单？");
+      if (yes) {
+        this.createOrder("待付款");
+        //提交订单信息
+        this.$axios.post("https://afc1gr.toutiao15.com/insertOrder", this.curOrder[0])
+          .then(res => {
+            alert("保存成功，请于待付款订单中查找~");
+            this.$router.push("/cart");
+          })
+          .catch(err => {
+            alert("保存失败！");
+            console.log(err);
+          });
+        next();
+      }
+      else {
+        next();
+        console.log(to);
+      }
+    } else {//已经下过单了，可以直接跳转
+      next();
+    }
+  },
+  computed: {
+    submitBarText() {
+      let count = 0;
+      this.products.forEach((shop) => {
+        if (shop.isSelected) count++;
       })
-      .catch(err=>{
+      return '结算' + (count ? `(${count})` : '');
+    },
+    count() {
+      let count = 0;
+      this.products.forEach((shop) => {
+        if (shop.isSelected) count++;
+      })
+      return count;
+    },
+    totalPrice() {
+      let total = 0;
+      this.products.forEach((shop) => {
+        if (shop.isSelected) total += shop.cnum * shop.gprice;
+      })
+      return total * 100;
+    }
+  },
+
+  created() {
+    this.$axios.get("https://afc1gr.toutiao15.com/getDefaultAddr")
+      .then(res => {
+        //第一遍拿回来的数据是过期的
+        this.address = res.data.result;
+        //  console.log(this.address)
+        if (this.address.length >= 1) {
+          this.type = true;//有默认地址，就不显示添加地址
+          //var addr = document.getElementById("addrInfo");
+        }
+        else {
+          this.type = false;//否则需要添加地址
+        }
+        console.log(this.address);
+      })
+      .catch(err => {
         console.log(err);
       })
-      this.$axios.get("https://afc1gr.toutiao15.com/getOrderGoods")
-      .then(res=>{
+    this.$axios.get("https://afc1gr.toutiao15.com/getOrderGoods")
+      .then(res => {
         //第一次获取的数据过时了
       })
-      this.$axios.get("https://afc1gr.toutiao15.com/getOrderGoods")
-      .then(res=>{
-          this.products = res.data.result;
-          //console.log(this.products);
+    this.$axios.get("https://afc1gr.toutiao15.com/getOrderGoods")
+      .then(res => {
+        this.products = res.data.result;
+        //console.log(this.products);
       })
-      .catch(err=>{
-          console.log("下单商品获取失败",err)
+      .catch(err => {
+        console.log("下单商品获取失败", err)
       })
   }
 };
 </script>
 
-<style scoped>
+<style lang='less' scoped>
 .order {
   font-size: 14px;
   background: #f7f7f7;
@@ -287,7 +305,7 @@ computed: {
       color: red;
     }
   }
-  
+
   .van-submit-bar__bar {
     border-top: 1px solid #f7f7f7;
   }
@@ -306,7 +324,7 @@ computed: {
       line-height: 12px;
       margin-right: 5px;
     }
-    
+
     .price {
       color: #e93b3d;
       font-size: 10px;
@@ -317,6 +335,6 @@ computed: {
   }
 }
 .van-card__num {
-    float: left;
+  float: left;
 }
 </style>
